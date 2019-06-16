1fb2a2f0c87d85deb542cf6d1e5283a5b1c09c7c
laoshufeifei <laoshufeifei@qq.com>
2019-06-16 17:09:54 +0800

    init

---
+++ go_env_for_vscode.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+projs="
+github.com/uudashr/gopkgs/cmd/gopkgs
+github.com/sqs/goreturns
+github.com/mdempsky/gocode
+github.com/ramya-rao-a/go-outline
+github.com/acroca/go-symbols
+golang.org/x/tools/cmd/guru
+golang.org/x/tools/cmd/gorename
+github.com/rogpeppe/godef
+golang.org/x/lint/golint
+github.com/stamblerre/gocode
+github.com/go-delve/delve/cmd/dlv
+"
+
+for p in $projs;do
+	goget -u $p
+done
---
+++ init_ubuntu.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+installFish=1
+mail="laoshufeifei@qq.com"
+
+function checkAndInstall()
+{
+    for b in $@; do
+        if [[ -z `which $b` ]];then
+            sudo apt install -y $b
+            if [[ $? != 0 ]];then
+                sudo apt update
+                sudo apt install -y $b
+            fi
+        fi
+
+        if [[ -z `which $b` ]];then
+            echo $b had not install
+            exit
+        fi
+    done
+}
+
+# first should install wget and python
+checkAndInstall wget python
+
+wget https://tuna.moe/oh-my-tuna/oh-my-tuna.py -O oh-my-tuna.py
+sudo python oh-my-tuna.py --global
+
+if [[ $installFish ]];then
+    sudo apt-add-repository ppa:fish-shell/release-3 
+fi
+sudo apt update
+
+sudo apt install -y git cmake make gcc g++ gdb vim htop
+sudo apt install -y lrzsz tig screen ccache dos2unix unzip
+sudo apt install -y openssl openssh-client openssh-server libssl-dev
+sudo apt install -y curl libcurl4-openssl-dev python-dev  
+# sudo apt install -y automake libmad0-dev libmad0  p7zip-full libgles2-mesa-dev
+
+# python and pip
+wget https://bootstrap.pypa.io/get-pip.py -O get-pip.py && sudo python get-pip.py
+sudo python oh-my-tuna.py --global
+sudo pip install -U colorama pip lxml requests beautifulsoup4 pymongo psutil
+
+# fish
+if [[ $installFish ]];then
+    sudo apt-get install -y fish
+fi
+
+ssh-keygen -t rsa -C $mail -b 4096 && cat ~/.ssh/id_rsa.pub 
