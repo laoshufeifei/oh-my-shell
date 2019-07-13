9fd73fd451ecccc665a4a7c83e05ce1be637e5b5
laoshufeifei <laoshufeifei@qq.com>
2019-07-13 15:42:37 +0800

    update

--- init_ubuntu.sh
+++ init_ubuntu.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
-installFish=1
-mail="laoshufeifei@qq.com"
+read -p "install fish? 1/0 > "
+installFish=$REPLY
+if [[ $installFish == 1 ]];then
+    echo "will install fish"
+else
+    echo "will not install fish"
+fi
+
+if [[ ! -f ~/.gitconfig ]];then
+    read -p "input your name for git config > "
+    name=$REPLY
+
+    read -p "input your mail for git config > "
+    mail=$REPLY
+    echo "name is $name"
+    echo "mail is $mail"
+fi
 
 function installDependents()
 {
@@ -27,7 +42,7 @@ installDependents wget python
 wget https://tuna.moe/oh-my-tuna/oh-my-tuna.py -O oh-my-tuna.py
 sudo python oh-my-tuna.py --global
 
-if [[ $installFish ]];then
+if [[ $installFish == 1 ]];then
     sudo apt-add-repository ppa:fish-shell/release-3 
 fi
 sudo apt update
@@ -36,7 +51,7 @@ sudo apt install -y git cmake make gcc g++ gdb vim htop
 sudo apt install -y lrzsz tig screen ccache dos2unix unzip
 sudo apt install -y openssl openssh-client openssh-server libssl-dev
 sudo apt install -y curl libcurl4-openssl-dev python-dev  
-# sudo apt install -y automake libmad0-dev libmad0 p7zip-full libgles2-mesa-dev
+sudo apt install -y automake libmad0-dev libmad0 p7zip-full libgles2-mesa-dev
 
 # python and pip
 wget https://bootstrap.pypa.io/get-pip.py -O get-pip.py && sudo python get-pip.py
@@ -44,8 +59,14 @@ sudo pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 sudo pip install -U colorama pip lxml requests beautifulsoup4 pymongo psutil
 
 # fish
-if [[ $installFish ]];then
+if [[ $installFish == 1 ]];then
     sudo apt-get install -y fish
 fi
 
-ssh-keygen -t rsa -C $mail -b 4096 && cat ~/.ssh/id_rsa.pub 
+if [[ ! -f ~/.gitconfig ]];then
+    git config --global user.name $name
+    git config --global user.email $mail
+fi
+if [[ ! -f ~/.ssh/id_rsa.pub ]]
+    ssh-keygen -t rsa -C $mail -b 4096 && cat ~/.ssh/id_rsa.pub 
+fi
