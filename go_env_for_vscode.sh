a89553169b48d2f314b42d3ed112bb734ec9b0c6
laoshufeifei <laoshufeifei@qq.com>
2019-07-13 20:14:28 +0800

    update go tools

--- go_env_for_vscode.sh
+++ go_env_for_vscode.sh
@@ -1,23 +1,34 @@
 #!/bin/bash
 
-# export http_proxy=socks5://127.0.0.1:1080
-# export https_proxy=socks5://127.0.0.1:1080
-
-export GO111MODULE=on
-export GOPROXY=https://goproxy.io
+useHttpProxy=0
+if [[ $useHttpProxy == 1 ]];then
+    echo "use http proxy"
+    export http_proxy=socks5://127.0.0.1:1080
+    export https_proxy=socks5://127.0.0.1:1080
+else
+    echo "use GOPROXY"
+    export GO111MODULE=on
+    export GOPROXY=https://goproxy.io
+fi
 
 projs="
-github.com/uudashr/gopkgs/cmd/gopkgs
-github.com/sqs/goreturns
 github.com/mdempsky/gocode
+github.com/uudashr/gopkgs/cmd/gopkgs
 github.com/ramya-rao-a/go-outline
 github.com/acroca/go-symbols
 golang.org/x/tools/cmd/guru
 golang.org/x/tools/cmd/gorename
-github.com/rogpeppe/godef
-golang.org/x/lint/golint
-github.com/stamblerre/gocode
 github.com/go-delve/delve/cmd/dlv
+github.com/stamblerre/gocode
+github.com/rogpeppe/godef
+github.com/sqs/goreturns
+golang.org/x/lint/golint
+github.com/cweill/gotests/...
+github.com/fatih/gomodifytags
+github.com/josharian/impl
+github.com/davidrjenni/reftools/cmd/fillstruct
+github.com/haya14busa/goplay/cmd/goplay
+github.com/godoctor/godoctor
 "
 
 # check GOPATH
@@ -34,20 +45,18 @@ if [[ $? == 0 ]];then
     echo $GOPATH
 fi
 
-
-cd $GOPATH
+cd $GOPATH/src > /dev/null
 for p in $projs;do
     echo go get -u -v $p
     go get -u -v $p
     echo -e "\n\n\n"
 done
-cd -
-
+cd - > /dev/null
 
 
 # https://github.com/Microsoft/vscode-go/issues/2024
 # cp godef godef-gomod
-cd $GOPATH/bin
+cd $GOPATH/bin > /dev/null
 if [[ ! -f gocode-gomod && ! -f gocode-gomod.exe ]];then
     if [[ -f gocode.exe ]];then
         cp gocode.exe gocode-gomod.exe
@@ -55,4 +64,4 @@ if [[ ! -f gocode-gomod && ! -f gocode-gomod.exe ]];then
         cp gocode gocode-gomod
     fi
 fi
-cd -
+cd - > /dev/null
